f3e76626ddf3248e79695eedcfbefebf040de1f6
Amr Ibrahim <amramr3434@gmail.com>
2024-10-10 00:07:10 +0300

    progress in swagger

--- src/templates/backend/laravel/template/app/Http/Controllers/Controller.php
+++ src/templates/backend/laravel/template/app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Your API Name",
+ *     description="A description of your API"
+ * )
+ * @OA\Server(
+ *     description="Your server description",
+ *     url="http://your-api-domain.com/api/v1"
+ * )
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
